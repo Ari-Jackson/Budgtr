a5d50c24ff82bfb91cca66222633c5fd7d853e82
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-04-18 14:26:20 -0400

    Adds fetch to single transaction

--- src/pages/Transaction.tsx
+++ src/pages/Transaction.tsx
@@ -1,6 +1,7 @@
 import { Link, useParams } from "react-router-dom";
-import { type dataAsProps } from "./Home";
 import { type dataItem } from "./Home";
+import { useQuery, useQueryClient, useMutation } from "react-query";
+import { useNavigate } from "react-router-dom";
 
 const findMatchingTransaction = (id: string) => (transaction: dataItem) => {
   return transaction.id === Number(id);
@@ -8,22 +9,47 @@ const findMatchingTransaction = (id: string) => (transaction: dataItem) => {
 
 export { findMatchingTransaction };
 
-export default function Transaction({ data }: dataAsProps) {
+export default function Transaction() {
   const { id } = useParams();
-  if (!id) {
-    return <div>Error something went wrong</div>;
+  const navigate = useNavigate();
+  const {
+    isLoading,
+    error,
+    data: matchingTransaction,
+  } = useQuery(["transactions", id], async () => {
+    const res = await fetch(`http://localhost:3005/transactions/${id}`);
+    return await res.json();
+  });
+  const queryClient = useQueryClient();
+  const { mutate } = useMutation({
+    mutationFn: async () => {
+      const res = await fetch(`http://localhost:3005/transactions/${id}`, {
+        method: "DELETE",
+      });
+      return await res.json();
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["transactions"] });
+      navigate(`/`);
+    },
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
   }
-  const matchingTransaction = data.find(findMatchingTransaction(id));
-  if (!matchingTransaction) {
-    return <div>Error something went wrong</div>;
+
+  if (error) {
+    return <div>There was an error!</div>;
   }
+
+  const handleDelete = async () => mutate();
 
   return (
     <main className="flex min-h-screen flex-col items-center bg-gray-100 pt-28">
       <div className=" min-h-[18rem] w-4/5 max-w-7xl rounded-md bg-gray-200 p-5 shadow sm:w-3/5">
         <div className="mb-6 flex flex-col items-center justify-between space-y-6">
           <h1 className="text-4xl font-bold text-gray-900">
-            ${matchingTransaction.amout.toFixed(2)}
+            ${matchingTransaction.amount.toFixed(2)}
           </h1>
           <p className="text-md text-gray-500">
             From: {matchingTransaction.from}
@@ -46,7 +72,10 @@ export default function Transaction({ data }: dataAsProps) {
               Edit
             </button>
           </Link>
-          <button className="rounded-md border bg-red-500 p-2 px-4 text-white hover:border-red-500 hover:bg-white hover:text-red-500 sm:px-6">
+          <button
+            onClick={handleDelete}
+            className="rounded-md border bg-red-500 p-2 px-4 text-white hover:border-red-500 hover:bg-white hover:text-red-500 sm:px-6"
+          >
             Delete
           </button>
         </div>
